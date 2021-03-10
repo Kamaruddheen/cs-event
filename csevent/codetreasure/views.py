9b13eb2a3676e98cf504e1f55076a1d7787aeb17
harishsoccer7 <harishsoccer7@gmail.com>
2021-03-10 19:44:28 +0530

    cheated expected bug fixed

--- csevent/codetreasure/views.py
+++ csevent/codetreasure/views.py
@@ -26,9 +26,14 @@ def prelm_question(request):
             #Before sumitting the answer this if checks whether the time exceeds or not
             current_time_during_submit=datetime.datetime.now()
             if start<=current_time_during_submit and current_time_during_submit<=end:
-                obj = Stud_Res_CodeTreasure_Prelm.objects.create(
-                student=student, question=question, user_answer=user_answer, status=status)
-                return JsonResponse({'save': "completed"})
+                #Checking for cheated
+                current_user_obj=get_object_or_404(prelim_test,Student=request.user,event='codetreasure')
+                if current_user_obj.test_status=='cheated':
+                    return render(request,'show_test.html',{'status':'cheated'})
+                else:
+                    obj = Stud_Res_CodeTreasure_Prelm.objects.create(
+                    student=student, question=question, user_answer=user_answer, status=status)
+                    return JsonResponse({'save': "completed"})
             else: 
                 return JsonResponse({'save':'after'})
     
@@ -39,10 +44,10 @@ def prelm_question(request):
             return render(request,'show_test.html',{'start':start,'end':end,'status':'before'})
             #status = True indicates test still exist
         elif start<=current and current<=end:
-            current_user_obj=get_object_or_404(prelim_test,Student=request.user)
+            current_user_obj=get_object_or_404(prelim_test,Student=request.user,event='codetreasure')
             #Update prelim_test model for the particular user
             if current_user_obj.test_status=="not_started":
-                prelim_test.objects.filter(Student=request.user).update(start=datetime.datetime.now(),test_status='started',attended=True)
+                prelim_test.objects.filter(Student=request.user,event='codetreasure').update(start=datetime.datetime.now(),test_status='started',attended=True)
             elif current_user_obj.test_status=="cheated":
                 return HttpResponse('cheated') 
 
@@ -91,9 +96,13 @@ def final_code_shuffle_function(request):
             #Checks whether the time is exhausted or not before submitting the answers
             current_time_during_submit=datetime.datetime.now()
             if start<=current_time_during_submit and current_time_during_submit<=end:
-                obj = final_answer_relation.objects.create(
-                student=student, final_code_shuffle_question=question, user_answer=user_answer)
-                return JsonResponse({'result': "completed"})
+                current_user_obj=get_object_or_404(prelim_test,Student=request.user,event='codetreasure')
+                if current_user_obj.test_status=='cheated':
+                    return render(request,'show_test.html',{'status':'cheated'})
+                else:
+                    obj = final_answer_relation.objects.create(
+                            student=student, final_code_shuffle_question=question, user_answer=user_answer)
+                    return JsonResponse({'result': "completed"})
             else: 
                 return JsonResponse({'result':'after'})
 
@@ -105,10 +114,10 @@ def final_code_shuffle_function(request):
             return render(request,'show_test.html',{'start':start,'end':end,'status':'before'})
         
         elif start<=current and current<=end:
-            current_user_obj=get_object_or_404(final_test,student=request.user)
+            current_user_obj=get_object_or_404(final_test,student=request.user,event='codetreasure')
             #Update final_test model for the particular user
             if current_user_obj.test_status=="not_started":
-                final_test.objects.filter(student=request.user).update(start=datetime.datetime.now(),test_status='started',attended=True)
+                final_test.objects.filter(student=request.user,event='codetreasure').update(start=datetime.datetime.now(),test_status='started',attended=True)
             elif current_user_obj.test_status=="cheated":
                 return HttpResponse('cheated') 
 
@@ -129,7 +138,7 @@ def final_code_shuffle_function(request):
 
             return render(request, 'codetreasure/final_code_shuffle.html', context=context)
         elif end<current:
-            #test = False indicates test has completed already
+            #test = after indicates test has completed already
             return render(request,'show_test.html',{'start':start,'end':end,'status':'after'})
         else:
             return HttpResponse("something wrong sorry for the inconvenience")
@@ -151,9 +160,13 @@ def final_binary_function(request):
             user_answer = request.POST.get('answer', 'something wrong')
             current_time_during_submit=datetime.datetime.now()
             if start<=current_time_during_submit and current_time_during_submit<=end:
-                final_answer_relation.objects.create(
-                    student=student, final_code_binary_question=question, user_answer=user_answer)
-                return JsonResponse({'result': 'completed'})
+                current_user_obj=get_object_or_404(final_test,Student=request.user,event='codetreasure')
+                if current_user_obj.test_status=='cheated':
+                    return render(request,'show_test.html',{'status':'cheated'})
+                else:
+                    final_answer_relation.objects.create(
+                        student=student, final_code_binary_question=question, user_answer=user_answer)
+                    return JsonResponse({'result': 'completed'})
             else:
                 return JsonResponse({'result':'after'})
             
@@ -164,10 +177,10 @@ def final_binary_function(request):
         if start>current:
             return render(request,'show_test.html',{'start':start,'end':end,'status':'before'})
         elif start<=current and current<=end:
-            current_user_obj=get_object_or_404(final_test,student=request.user)
+            current_user_obj=get_object_or_404(final_test,student=request.user,event='codetreasure')
             #Update final_test model for the particular user
             if current_user_obj.test_status=="not_started":
-                final_test.objects.filter(student=request.user).update(start=datetime.datetime.now(),test_status='started',attended=True)
+                final_test.objects.filter(student=request.user,event='codetreasure').update(start=datetime.datetime.now(),test_status='started',attended=True)
             elif current_user_obj.test_status=="cheated":
                 return HttpResponse('cheated') 
             page_no = request.GET.get('page', 1)
@@ -205,9 +218,13 @@ def final_spot_error_function(request):
             user_answer = request.POST.get('answer', 'something went wrong')
             current_time_during_submit=datetime.datetime.now()
             if start<=current_time_during_submit and current_time_during_submit<=end:
-                final_answer_relation.objects.create(
-                    student=student, final_code_spot_error_question=question, user_answer=user_answer)
-                return JsonResponse({'result': 'completed'})
+                current_user_obj=get_object_or_404(prelim_test,Student=request.user,event='codetreasure')
+                if current_user_obj.test_status=='cheated':
+                    return render(request,'show_test.html',{'status':'cheated'})
+                else:
+                    final_answer_relation.objects.create(
+                        student=student, final_code_spot_error_question=question, user_answer=user_answer)
+                    return JsonResponse({'result': 'completed'})
             else:
                 return JsonResponse({'result':'after'})
     else:
@@ -217,10 +234,10 @@ def final_spot_error_function(request):
         if start>current:
             return render(request,'show_test.html',{'start':start,'end':end,'status':'before'})
         elif start<=current and current<=end:
-            current_user_obj=get_object_or_404(final_test,student=request.user)
+            current_user_obj=get_object_or_404(final_test,student=request.user,event='codetreasure')
              #Update final_test model for the particular user
             if current_user_obj.test_status=="not_started":
-                final_test.objects.filter(student=request.user).update(start=datetime.datetime.now(),test_status='started',attended=True)
+                final_test.objects.filter(student=request.user,event='codetreasure').update(start=datetime.datetime.now(),test_status='started',attended=True)
             elif current_user_obj.test_status=="cheated":
                 return HttpResponse('cheated') 
             page_no = request.GET.get('page', 1)
@@ -239,14 +256,14 @@ def final_spot_error_function(request):
             }
             return render(request, 'codetreasure/final_spot_error.html', context=context)
         elif end<current:
-            #test = False indicates test has completed already
+            #test = after indicates test has completed already
             return render(request,'show_test.html',{'start':start,'end':end,'status':'after'})
         else:
             return HttpResponse("something wrong sorry for the inconvenience")
 
 #Final question hendling view
 def last_question(request):
-    final_test.objects.filter(student=request.user).update(end=datetime.datetime.now(),test_status='finished')
+    final_test.objects.filter(student=request.user,event='code_treasure').update(end=datetime.datetime.now(),test_status='finished')
     return HttpResponse('Successful test completion')
 
 
